7d953474e093fa7f47c66e5047ef2676d245a915
Ícaro da Silva Santos <icarosantoss016@gmail.com>
2026-07-04 18:50:59 -0300

    update: module vitrine.js

--- Front-End/SmartRetail/src/store/index.js
+++ Front-End/SmartRetail/src/store/index.js
@@ -7,4 +7,4 @@ export default createStore({
         auth,
         vitrine
     }
-})
+})    
--- Front-End/SmartRetail/src/store/modules/vitrine.js
+++ Front-End/SmartRetail/src/store/modules/vitrine.js
@@ -149,3 +149,4 @@
     export default{
         namespaced: true,state, getters,mutations,actions
     }
+      
