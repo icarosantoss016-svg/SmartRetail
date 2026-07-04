eb6199354f21ef7059413b6b8fb2a35bdd422643
Ícaro da Silva Santos <icarosantoss016@gmail.com>
2026-07-04 18:47:13 -0300

    Inclusão do module vitrine.js

--- Front-End/SmartRetail/src/store/index.js
+++ Front-End/SmartRetail/src/store/index.js
@@ -1,8 +1,10 @@
 import { createStore } from 'vuex'
 import auth from './modules/auth'
+import vitrine from '.modules/vitrine'
 
 export default createStore({
     modules:{
-        auth
+        auth,
+        vitrine
     }
 })
